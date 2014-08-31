8f63c398b4cb60e0615edb542b35b39128feeae9
alexanderkenndy <alexander.kenndy@gmail.com>
2014-08-31 16:52:34 +0800

    init unit test file

---
+++ test/unit/user.test.js
@@ -0,0 +1,18 @@
+describe('logistics_server_user test', function() {
+	/**
+	 * pre condition
+	 * similar to setup
+	 */
+	beforeEach(function() {
+	});
+
+	/**
+	 * after this module'test
+	 * similar to teardown
+	 */
+	afterEach(function() {
+
+	});
+	it('', function() {
+	});
+});
